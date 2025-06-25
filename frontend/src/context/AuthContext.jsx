e26bd2cb5cee4099f4d6da6afc8c48422bd1516d
Jaiprakash Yadav <jpy7260@gmail.com>
2025-06-26 02:05:57 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext } from 'react'
 export const authDataContext=createContext()
 function AuthContext({children}) {
-const serverUrl="https://linkedin-app-backend-final-deployment.onrender.com"
+const serverUrl="https://linkedin-app-backend-2c5a.onrender.com"
     let value={
         serverUrl
     }
